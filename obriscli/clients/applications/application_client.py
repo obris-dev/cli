735b7d3ece1b07189afe7ab686135c6deffd5a95
Josh Panka <josh.panka@gmail.com>
2023-12-09 17:11:52 -0500

    feature: human readable cloud provider (#17)
    * human readable cloud provider
    
    * remove ipdb

--- obriscli/clients/applications/application_client.py
+++ obriscli/clients/applications/application_client.py
@@ -1,10 +1,22 @@
+from enum import Enum
+
 from ..base_client import BaseRESTClient
 
 from .application_response_mapper import ApplicationResponseMapper
 from .routes import ApplicationPath
 
 
+class ApplicationType(Enum):
+    AWS = 0
+    AZURE = 1
+    GCP = 2
+
+
 class ApplicationClient(BaseRESTClient):
+
+    @staticmethod
+    def __map_provider_type(provider_slug):
+        return ApplicationType[provider_slug.upper()].value
 
     def list(self, has_credentials=None):
         query_params = {}
@@ -29,11 +41,17 @@ class ApplicationClient(BaseRESTClient):
         formatted_response = ApplicationResponseMapper.application(application)
         return formatted_response
 
-    def create(self, name=None, region=None, description=None):
-        if name is None or region is None:
-            raise ValueError("Missing required argument 'name' or 'region'.")
+    def create(self, name=None, provider=None, region=None, description=None):
+        if name is None:
+            raise ValueError("missing name.")
+        if provider is None:
+            raise ValueError("missing provider.")
+        if region is None:
+            raise ValueError("missing region.")
 
+        provider_type = self.__map_provider_type(provider)
         data = {
+            "type": provider_type,
             "name": name,
             "region": region,
             "description": description,
@@ -70,3 +88,14 @@ class ApplicationClient(BaseRESTClient):
         application_path_format_str = ApplicationPath.APPLICATION.value
         application_path = application_path_format_str.format(pk)
         return super().delete(application_path)
+
+    def regions(self, provider=None):
+        if provider is None:
+            raise ValueError("missing provider.")
+
+        regions_path_format_str = ApplicationPath.REGIONS.value
+        region_path = regions_path_format_str.format(provider)
+
+        response_json = self.get(region_path)
+        regions = response_json["regions"]
+        return regions
--- obriscli/clients/applications/routes.py
+++ obriscli/clients/applications/routes.py
@@ -4,3 +4,4 @@ from enum import Enum
 class ApplicationPath(Enum):
     APPLICATIONS = "/applications/"
     APPLICATION = "/applications/{}"
+    REGIONS = "/applications/regions/{}"
